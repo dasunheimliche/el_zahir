885a21422c40f16fb484f110a4594b1d5928a1fa
claussimar <dasunheimliche7@gmail.com>
2025-02-02 03:53:17 -0300

    improving prisma schema and folder structure

--- zahir-backend-v2/src/app.ts
+++ zahir-backend-v2/src/app.ts
@@ -2,7 +2,6 @@ import express from "express";
 import dotenv from "dotenv";
 import swaggerUi from "swagger-ui-express";
 import { specs } from "./shared/config/swagger";
-import { helloController } from "./components/hello/entry-points/api/hello.controller";
 
 dotenv.config();
 
@@ -13,8 +12,6 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(specs));
-
-app.use("/api/hello", helloController);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
